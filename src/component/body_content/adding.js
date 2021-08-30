22370f503565d5a88a29a01d52c4b1be82e638da
jayPrakash10 <79151172+jayPrakash10@users.noreply.github.com>
2021-08-30 15:53:18 +0530

    Add files via upload

--- src/component/body_content/adding.js
+++ src/component/body_content/adding.js
@@ -1,5 +1,15 @@
 import books from './books.js'
 
+function change(warn){
+    if(warn=='yw'){
+        var t = document.getElementById(warn);
+        t.style.visibility='hidden'    
+    }
+    else{
+        var t = document.getElementById(warn);
+        t.style.display='none'
+    }
+}
 function add(){
     const title=document.getElementById("title").value;
     const subt=document.getElementById("subtitle").value;
@@ -15,10 +25,23 @@ function add(){
         ph=ar[ar.length-1]
     }
 
-    if(title==='' || subt==='' || author==='' || year==='' || isNaN(price) || desc==='')
+    if(title==='' || author==='' || year==='' || isNaN(price) || desc==='')
     {
-        alert("Fields are empty");
-
+        if(title==''){
+            var t = document.getElementById('tw')
+            t.style.display='block'}
+        if(author==''){    
+            var t = document.getElementById('aw')
+            t.style.display='block'}
+        if(year==''){
+            var t = document.getElementById('yw')
+            t.style.display='block'}
+        if(isNaN(price)){
+            var t = document.getElementById('pw')
+            t.style.display='block'}
+        if(desc==''){
+            var t = document.getElementById('dw')
+            t.style.display='block'}
     }
     else{
             const b={
@@ -46,4 +69,4 @@ function add(){
     }    
 }
 
-export default add;
+export {change,add};
