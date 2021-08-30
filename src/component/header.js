880b7bab47652e614c2f2845d39c31918ba2d2fa
jayPrakash10 <79151172+jayPrakash10@users.noreply.github.com>
2021-08-30 15:43:58 +0530

    Add files via upload

--- src/component/header.js
+++ src/component/header.js
@@ -2,19 +2,30 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 function Header(){
+
+    function myfunction(){
+        var op = document.getElementById('options');
+        if(op.style.display==='block')
+            op.style.display='none';
+        else
+            op.style.display='block';
+    }
+
     return(<>
             <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-                <div className="container-fluid">
-                    <ul className="navbar-nav mr-auto">
-                        <li className="nav-item">
-                            <Link className="nav-link" to='/get'>Getbooks</Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link className="nav-link" to='/add'>Addbooks</Link>
-                        </li>
-                    </ul>
+                <div className="container-fluid myNav" id='mynav' style={{justifyContent:'flex-end'}}>
+                    <Link className="nav-link" to='/get' style={{color:'rgba(255,255,255,0.55)'}}>Getbooks</Link>
+                    <Link className="nav-link" to='/add' style={{color:'rgba(255,255,255,0.55)'}}>Addbooks</Link>
+                    <div className='myicon' onClick={myfunction}>
+                        <i className="bi bi-chevron-double-down" style={{color:'rgba(255,255,255,0.55)'}}></i>
+                    </div>
                 </div>
             </nav>
+            <div className='options' id='options'>
+                <Link className="nav-link" to='/get' style={{color:'rgba(255,255,255,0.55)'}}>Getbooks</Link>
+                <Link className="nav-link" to='/add' style={{color:'rgba(255,255,255,0.55)'}}>Addbooks</Link>
+            </div>
+
         </>
     );
 }
