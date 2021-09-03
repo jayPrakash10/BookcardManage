a6465d0e3ccd0e11c43d9ee4618b828ef3c22f1a
jayPrakash10 <79151172+jayPrakash10@users.noreply.github.com>
2021-09-03 21:04:24 +0530

    Add files via upload

--- src/component/body_content/showmodal.js
+++ src/component/body_content/showmodal.js
@@ -10,16 +10,13 @@ function ShowBookmodal(props){
     const clsbtn = React.createElement('button',{className:'btn close', value:bid, onClick:(e)=>{closebtn(e.target.value)}},"x");
     const header = React.createElement('div',{className:'modalheader'}, [clsbtn]);
     const photo= React.createElement('img',{className:'mcard-img', src:books[i].photo})
-    const titleinput=React.createElement('h1',{className:"", style:{marginLeft:'5px'}},books[i].title);
-    const title = React.createElement('div',{className:'input-group'},[titleinput])
-    const subtitleinput=React.createElement('h5',{className:"", style:{marginLeft:'5px'}},books[i].subbtitle);
-    const subtitle=React.createElement('div',{className:'input-group'},[subtitleinput]);
-    const authorinput=React.createElement('p',{className:"", style:{marginLeft:'5px'}},'by '+books[i].author);
-    const author = React.createElement('div',{className:'input-group'},[authorinput]);
-    const descinput=React.createElement('div',{className:"", style:{marginLeft:'5px'}},books[i].desc);
-    const desc = React.createElement('div',{className:'input-group'},[descinput])
+    const title=React.createElement('h1',{className:"input-group", style:{marginLeft:'5px'}},books[i].title);
+    const subtitle=React.createElement('h5',{className:'input-group', style:{marginLeft:'5px'}},books[i].subbtitle);
+    const author = React.createElement('p',{className:'input-group', style:{marginLeft:'5px'}},'by '+books[i].author);
+    const price = React.createElement('p',{style:{textAlign:'right'}},'Rs. '+books[i].price);
+    const desc=React.createElement('div',{className:"input-group", style:{marginLeft:'5px'}},books[i].desc);
     
-    const body = React.createElement('div',{className:'modalbody'},[photo,title,subtitle,author,desc]);
+    const body = React.createElement('div',{className:'modalbody'},[photo,title,subtitle,author,price,desc]);
 
     const footer = React.createElement('div',{className:'modalfooter'},React.createElement('h2',{},""))
     const  content = React.createElement('div',{className:"modalcontent"},[header,body,footer])
